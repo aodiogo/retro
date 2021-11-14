e5fdca5306c2803238b33be0cdc2704ea2f91a35
aodiogo <diogo.oliveira@gmail.com>
2021-11-14 18:04:59 -0500

    Add CommentServiceTest.java

---
+++ src/test/java/com/diogo/retro/service/CommentServiceTest.java
@@ -0,0 +1,86 @@
+package com.diogo.retro.service;
+
+import com.diogo.retro.model.Comment;
+import com.diogo.retro.model.CommentRepository;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+public class CommentServiceTest {
+
+    @MockBean
+    private CommentRepository commentRepository;
+
+    private CommentService commentService;
+
+    @Before
+    public void init() {
+        commentService = new CommentService(commentRepository);
+    }
+
+    @Test
+    public void
+    getAllCommentsForToday_HappyPath_ShouldReturn1Comment() {
+        // Given
+        Comment comment = new Comment();
+        comment.setComment("Test");
+        comment.setType(CommentType.PLUS);
+        comment.setCreatedDate(new
+                Timestamp(System.currentTimeMillis()));
+        List<Comment> comments = Arrays.asList(comment);
+        LocalDate now = LocalDate.now();
+
+
+        when(commentRepository.findByCreatedYearAndMonthAndDay(now.getYear(), now.getMonth().getValue(),
+                now.getDayOfMonth())).thenReturn(comments);
+
+        // When
+        List<Comment> actualComments =
+                commentService.getAllCommentsForToday();
+
+        // Then
+        verify(commentRepository,
+                times(1)).findByCreatedYearAndMonthAndDay(now.getYear(),
+                now.getMonth().getValue(), now.getDayOfMonth());
+        assertThat(comments).isEqualTo(actualComments);
+    }
+
+    @Test
+    public void saveAll_HappyPath_ShouldSave2Comments() {
+        // Given
+        Comment comment = new Comment();
+        comment.setComment("Test Plus");
+        comment.setType(CommentType.PLUS);
+        comment.setCreatedBy("Shazin");
+        comment.setCreatedDate(new
+                Timestamp(System.currentTimeMillis()));
+
+        Comment comment2 = new Comment();
+        comment2.setComment("Test Star");
+        comment2.setType(CommentType.STAR);
+        comment2.setCreatedBy("Shahim");
+        comment2.setCreatedDate(new
+                Timestamp(System.currentTimeMillis()));
+        List<Comment> comments = Arrays.asList(comment, comment2);
+        when(commentRepository.saveAll(comments)).thenReturn(comments);
+
+        // When
+        List<Comment> saved = commentService.saveAll(comments);
+
+        // Then
+        assertThat(saved).isNotEmpty();
+        verify(commentRepository, times(1)).saveAll(comments);
+
+    }
+}
